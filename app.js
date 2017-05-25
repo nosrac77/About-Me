2fedfbcc685f332647be0a3dcd0f2e30d32a2029
nosrac77 <carson.newton@outlook.com>
2017-05-25 04:33:41 -0400

    initial commit with project scaffolding

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var confirmed = confirm('Are you ready to rock and roll?!');
+
+if (confirmed) {
+  console.log('The user is ready');
+} else {
+  console.log('The user said they were not ready... but we are gonna start anyway.')
+}
