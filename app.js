dd90f416d237483bd3eea04540255ce3bd6c0823
nosrac77 <carson.newton@outlook.com>
2017-05-28 01:39:59 -0400

    updated fonts added more javascript

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (question1 === 'N') {
   alert('Seriously? Even after I said no bullshit? Come on man! Alright, you lose. 0 for 0 so far, happy now?');
 }
 
-var question2 = prompt('Ok, next up is an easy one. Is my favorite pair of shoes Nike brand or nah? Answer Y or N to submit a valid response... no tricks. Just Y or N. You can do this.');
+var question2 = prompt('Ok, next up is an easy one. Is my favorite pair of shoes Nike brand or nah? Answer Y or N to submit a valid response... no tricks. Just Y or N. You can do this.').toUpperCase();
 
 if (question2 === 'Y'){
   console.log('The user is correct. Correct as HELL.');
@@ -23,5 +23,17 @@ if (question2 === 'Y'){
   console.log('The user is dumb and also hates me.');
   alert('Goodness gracious. That was a lay-up! Come on, wrongy mcwrongface! Look alive!');
 } else {
-  alert('Dammit. Simple instructions yo, ya gotta follow em! 0 for 2 now. You\'re not doing hot right now.');
+  alert('Dammit. Simple instructions yo, ya gotta follow em! You\'re not doing hot right now.');
+}
+
+var question3 = prompt('Third question! See how easy this is? You\'re more than halfway done! Anyway, another easy one. Is my birthday next month? Again, answer Y or N. No shenanigans.').toUpperCase();
+
+if (question3 === 'N'){
+  console.log('The user is correct. Correct as HELL.');
+  alert('Haha, I knew we were best friends. I KNEW IT. You ditching me last Friday was a total fluke huh?! Anyway, enjoy being correct!');
+} else if (question3 === 'Y') {
+  console.log('The user is dumb and also hates me.');
+  alert('First answer you\'ve gotten wrong, chump? I honestly don\'t know, this JavaScript isn\'t sophisticated enough to tell! But I\'m willing to bet it isn\'t! Anyway, you\'re wrong!');
+} else {
+  alert('Wow... can\'t type a Y or an N? Too cool for school? Whatever, you get this one wrong for your defiance.');
 }
